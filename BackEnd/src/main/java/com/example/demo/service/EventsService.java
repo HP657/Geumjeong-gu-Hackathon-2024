75acd7beacb5ed1aa3c5ea1b14aa933d7a59bb22
HP657 <junlee5070@gmail.com>
2024-08-15 22:48:14 +0900

    서울관련 이벤트 가져오기

---
+++ BackEnd/src/main/java/com/example/demo/service/EventsService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.Response;
+import com.example.demo.entity.Events;
+import com.example.demo.repository.EventsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventsService {
+
+    @Autowired
+    private EventsRepository eventsRepository;
+
+    public Response<List<Events>> seoulEvents() {
+        return new Response<>(eventsRepository.findByRegion("서울시"), HttpStatus.OK);
+    }
+}
