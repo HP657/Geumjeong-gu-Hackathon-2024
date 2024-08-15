229adee8de91bd871279279c8dbf3cadd3e466ca
HP657 <junlee5070@gmail.com>
2024-08-15 22:48:02 +0900

    크롤링 시키는 서비스

---
+++ BackEnd/src/main/java/com/example/demo/service/CrawlEvents.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CrawlEvents {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Response<String> getEvents() {
+        String url = "http://localhost:5000/api/crawl";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        return new Response<>(response.getBody(), HttpStatus.OK);
+    }
+}
