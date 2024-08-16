7002936140305304b02621e4c54b8956ef7c03c0
HP657 <junlee5070@gmail.com>
2024-08-16 14:25:54 +0900

    이벤트 엔티티 TEXT 고정 및 변경

--- BackEnd/src/main/java/com/example/demo/entity/Events.java
+++ BackEnd/src/main/java/com/example/demo/entity/Events.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,13 +17,26 @@ public class Events {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long eventId;
 
+    @Column(columnDefinition = "TEXT")
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
+
+    @Column(columnDefinition = "TEXT")
     private String imgSrc;
+
+    @Column(columnDefinition = "TEXT")
     private String period;
+
+    @Column(columnDefinition = "TEXT")
     private String place;
+
+    @Column(columnDefinition = "TEXT")
     private String contact;
+
+    @Column(columnDefinition = "TEXT")
     private String region;
 }
