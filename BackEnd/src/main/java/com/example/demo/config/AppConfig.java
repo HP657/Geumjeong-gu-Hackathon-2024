8119b5987317ec6f7d13a8d13cc93c109929412f
HP657 <junlee5070@gmail.com>
2024-08-17 01:14:07 +0900

    cors 수정

--- BackEnd/src/main/java/com/example/demo/config/AppConfig.java
+++ BackEnd/src/main/java/com/example/demo/config/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5000")
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
