476c73eac972a5779ba88d91b3ccce5ec31c9517
HP657 <junlee5070@gmail.com>
2024-08-16 14:27:30 +0900

    각 지역별 컨트롤러 생성

--- BackEnd/src/main/java/com/example/demo/api/EventsApiController.java
+++ BackEnd/src/main/java/com/example/demo/api/EventsApiController.java
@@ -19,7 +19,87 @@ public class EventsApiController {
     private EventsService eventsService;
 
     @GetMapping("/seoul")
-    public ResponseEntity<Response<List<Events>>> seoulEvnets() {
-        return eventsService.seoulEvents().toResponseEntity();
+    public ResponseEntity<Response<List<Events>>> seoulEvents() {
+        return eventsService.getEventsByRegion("서울시").toResponseEntity();
+    }
+
+    @GetMapping("/busan")
+    public ResponseEntity<Response<List<Events>>> busanEvents() {
+        return eventsService.getEventsByRegion("부산시").toResponseEntity();
+    }
+
+    @GetMapping("/daegu")
+    public ResponseEntity<Response<List<Events>>> daeguEvents() {
+        return eventsService.getEventsByRegion("대구시").toResponseEntity();
+    }
+
+    @GetMapping("/incheon")
+    public ResponseEntity<Response<List<Events>>> incheonEvents() {
+        return eventsService.getEventsByRegion("인천시").toResponseEntity();
+    }
+
+    @GetMapping("/gwangju")
+    public ResponseEntity<Response<List<Events>>> gwangjuEvents() {
+        return eventsService.getEventsByRegion("광주시").toResponseEntity();
+    }
+
+    @GetMapping("/daejeon")
+    public ResponseEntity<Response<List<Events>>> daejeonEvents() {
+        return eventsService.getEventsByRegion("대전시").toResponseEntity();
+    }
+
+    @GetMapping("/ulsan")
+    public ResponseEntity<Response<List<Events>>> ulsanEvents() {
+        return eventsService.getEventsByRegion("울산시").toResponseEntity();
+    }
+
+    @GetMapping("/sejong")
+    public ResponseEntity<Response<List<Events>>> sejongEvents() {
+        return eventsService.getEventsByRegion("세종시").toResponseEntity();
+    }
+
+    @GetMapping("/gyeonggi")
+    public ResponseEntity<Response<List<Events>>> gyeonggiEvents() {
+        return eventsService.getEventsByRegion("경기도").toResponseEntity();
+    }
+
+    @GetMapping("/gangwon")
+    public ResponseEntity<Response<List<Events>>> gangwonEvents() {
+        return eventsService.getEventsByRegion("강원도").toResponseEntity();
+    }
+
+    @GetMapping("/chungbuk")
+    public ResponseEntity<Response<List<Events>>> chungbukEvents() {
+        return eventsService.getEventsByRegion("충청북도").toResponseEntity();
+    }
+
+    @GetMapping("/chungnam")
+    public ResponseEntity<Response<List<Events>>> chungnamEvents() {
+        return eventsService.getEventsByRegion("충청남도").toResponseEntity();
+    }
+
+    @GetMapping("/jeonbuk")
+    public ResponseEntity<Response<List<Events>>> jeonbukEvents() {
+        return eventsService.getEventsByRegion("전라북도").toResponseEntity();
+    }
+
+    @GetMapping("/jeonnam")
+    public ResponseEntity<Response<List<Events>>> jeonnamEvents() {
+        return eventsService.getEventsByRegion("전라남도").toResponseEntity();
+    }
+
+    @GetMapping("/gyeongbuk")
+    public ResponseEntity<Response<List<Events>>> gyeongbukEvents() {
+        return eventsService.getEventsByRegion("경상북도").toResponseEntity();
+    }
+
+    @GetMapping("/gyeongnam")
+    public ResponseEntity<Response<List<Events>>> gyeongnamEvents() {
+        return eventsService.getEventsByRegion("경상남도").toResponseEntity();
+    }
+
+    @GetMapping("/jeju")
+    public ResponseEntity<Response<List<Events>>> jejuEvents() {
+        return eventsService.getEventsByRegion("제주도").toResponseEntity();
     }
 }
