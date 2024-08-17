2fc26088e2e5de42f9b9cc74d9b7ae12195655e0
HP657 <junlee5070@gmail.com>
2024-08-17 14:03:55 +0900

    로그인 중인지 로그인 할것인지 템플릿 수정

--- FrontEnd/main/js/loging2.js
+++ FrontEnd/main/js/loging2.js
@@ -4,7 +4,7 @@ fetch("http://localhost:8080/api/auth/info", {
 })
   .then((response) => {
     if (!response.ok) {
-      throw new Error("Not authenticated");
+      thrownewError("Not authenticated");
     }
     return response.json();
   })
@@ -12,9 +12,14 @@ fetch("http://localhost:8080/api/auth/info", {
     console.log(data);
     const authStatus = document.getElementById("loging");
     authStatus.textContent = `${data.data.username} 로그인 중`;
+
     const logouting = document.getElementById("logout");
     logouting.textContent = "로그아웃";
   })
   .catch((error) => {
     console.error("Error:", error);
+    const authStatus = document.getElementById("loging");
+
+    authStatus.href = "/main/SignInPage.html";
+    authStatus.textContent = "로그인하기";
   });
